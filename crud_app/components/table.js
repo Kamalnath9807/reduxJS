6d43ea7392db291b70fa3d684c3f038f670901e0
kamalnath <kamalnath.srinivasan@tridentsqa.com>
2022-12-27 17:50:14 +0530

    added form and table in CRUD application

--- crud_app/components/table.js
+++ crud_app/components/table.js
@@ -1,3 +1,5 @@
+import { BiEdit, BiTrashAlt } from 'react-icons/bi';
+
 export default function Table() {
   return (
     <table className='min-w-full table-auto'>
@@ -23,7 +25,38 @@ export default function Table() {
           </th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody className='bg-gray-200'>
+        <tr className='bg-gray-50 text-center'>
+          <td className='px-16 py-2 flex-row items-center'>
+            <img src='#' alt='' />
+            <span className='text-center ml-2 font-semibold'>Daily Tution</span>
+          </td>
+          <td className='px-16 py-2'>
+            <span>dailytution@gmail.com</span>
+          </td>
+          <td className='px-16 py-2'>
+            <span>$25000</span>
+          </td>
+          <td className='px-16 py-2'>
+            <span>10-05-2022</span>
+          </td>
+          <td className='px-16 py-2'>
+            <button className='cursor'>
+              <span className='bg-green-500 text-white px-5 py-1 rounded'>
+                Active
+              </span>
+            </button>
+          </td>
+          <td className='px-16 py-2 flex justify-around gap-5'>
+            <button className='cursor'>
+              <BiEdit size={25} color={'rgb(34,197,94'} />
+            </button>
+            <button className='cursor'>
+              <BiTrashAlt size={25} color={'rgb(244,63,94'} />
+            </button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   );
 }
