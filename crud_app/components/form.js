2a0f17fa984be2841f791636647aecd6637633dc
kamalnath <kamalnath.srinivasan@tridentsqa.com>
2022-12-28 16:52:49 +0530

    updated CRUD project

--- crud_app/components/form.js
+++ crud_app/components/form.js
@@ -1,9 +1,36 @@
+import { useReducer } from 'react';
+import { BiPlus } from 'react-icons/bi';
+import Bug from './bug';
+import Success from './success';
+
+//state: we get previous state and event current dispatch value
+//event.target.value denotes the input text box below form
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
+
 export default function Form() {
+  //hook it allows you to get the data from form. It is similar to useState() in react. in this case it returns current state and dispatch function
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Object.keys(formData).length == 0)
+      return console.log("Don't have Form Data");
+    console.log(formData);
+  };
+
+  if (Object.keys(formData).length > 0) return <Bug message={'Success'} />;
+
   return (
-    <form className='grid lg:grid-cols-2 w-4/6 gap-4'>
+    <form className='grid lg:grid-cols-2 w-4/6 gap-4' onSubmit={handleSubmit}>
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='firstname'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='FirstName'
@@ -12,6 +39,7 @@ export default function Form() {
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='lastname'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='Lastname'
@@ -20,6 +48,7 @@ export default function Form() {
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='email'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='Email'
@@ -28,6 +57,7 @@ export default function Form() {
       <div className='input-type'>
         <input
           type='text'
+          onChange={setFormData}
           name='firstname'
           className='border w-full px-5 py-3 focus:outline-none rounded-md'
           placeholder='Salary'
@@ -36,6 +66,7 @@ export default function Form() {
       <div className='input-type'>
         <input
           type='date'
+          onChange={setFormData}
           name='date'
           className='border px-5 py-3 focus:outline-none rounded-md'
           placeholder='date'
@@ -46,6 +77,7 @@ export default function Form() {
         <div className='form-check'>
           <input
             value='Active'
+            onChange={setFormData}
             id='radioDefault1'
             type='radio'
             name='status'
@@ -58,6 +90,7 @@ export default function Form() {
         <div className='form-check'>
           <input
             value='Inactive'
+            onChange={setFormData}
             id='radioDefault2'
             type='radio'
             name='status'
@@ -70,7 +103,10 @@ export default function Form() {
       </div>
 
       <button className='flex justify-center text-md w-2/6  bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500'>
-        Add
+        Add{' '}
+        <span className='px-1'>
+          <BiPlus size={24} />
+        </span>
       </button>
     </form>
   );
