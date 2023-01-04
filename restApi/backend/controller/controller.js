5cb16f9ddd05108e8b835f6ec7dcce5da42f32c2
kamalnath <kamalnath.srinivasan@tridentsqa.com>
2023-01-04 17:43:26 +0530

    added auth

--- restApi/backend/controller/controller.js
+++ restApi/backend/controller/controller.js
@@ -1,33 +1,60 @@
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/models');
+
 // @desc  Get goals
 // @route GET /api/goals
 // @access Private
-const getGoal = (req, res) => {
-  res.status(200).json({ message: 'create Alldetails' });
-};
+const getGoal = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
+});
 
 // @desc  Set goals
 // @route POSt /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error('please add a text field');
   }
-  res.status(200).json({ message: 'Post Alldetails' });
-};
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
+});
 
 // @desc  Update goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
-};
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goals not found');
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 
 // @desc  Delete goals
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+const deleteGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+  if (!goal) {
+    res.status(400);
+    throw new Error('Goals not found');
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = { getGoal, setGoal, updateGoal, deleteGoal };
--- restApi/backend/server.js
+++ restApi/backend/server.js
@@ -1,16 +1,21 @@
 const routers = require('./router/router');
+const userRouters = require('./router/userRouters');
+const colors = require('colors');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', routers);
-
+app.use('/api/auth', routers);
+app.use('/api/userauth', userRouters);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
